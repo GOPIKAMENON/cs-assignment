c76f424ad854d6a8f0a15ebb8f35a61bff7c49a1
GOPIKAMENON <31345164+GOPIKAMENON@users.noreply.github.com>
2017-08-25 20:46:19 +0530

    Add files via upload

---
+++ Lab1.q6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c,d;
+cout<<"enter radius";
+cin>>a;
+c=2*a;
+cout<<"the diameter is"<<c<<endl;
+d=2*3.14*a;
+cout<<"the circumference is"<<d<<endl;
+b=3.14*a*a;
+cout<<"the area is"<<b<<endl;
+return 0;
+}
