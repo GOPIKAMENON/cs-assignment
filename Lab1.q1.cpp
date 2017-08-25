39f1a2c66b8d42bfca6c1874290fa927212277fd
GOPIKAMENON <31345164+GOPIKAMENON@users.noreply.github.com>
2017-08-25 20:42:25 +0530

    Add files via upload

---
+++ Lab1.q1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	a=2;
+
+	double b,c,d;
+	b=3.47645;
+	c=4.7654;
+	d=b*c;
+
+	char e;
+	e='f';
+
+	bool boolean = true;
+
+	cout<<"character="<<e<<endl;
+	cout<<"the product "<<d<<endl;
+	cout << a<<endl;
+	cout << boolean<<endl;
+
+	return 0;
+}
