63dd159e1eb348aef0506f2101638441726c68e9
GOPIKAMENON <31345164+GOPIKAMENON@users.noreply.github.com>
2017-08-25 20:45:40 +0530

    Add files via upload

---
+++ Lab1.q4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout<<"enter length";
+cin>>a;
+cout<<"enter breadth";
+cin>>b;
+c=2*a+2*b;
+cout<<"the perimeter is"<<c<<endl;
+return 0;
+}
+         
